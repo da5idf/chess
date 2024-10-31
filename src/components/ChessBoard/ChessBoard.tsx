24952acfb31b80619a134f078accbd6e27318d0a
davidforster <david.forster@nowsta.com>
2024-10-31 11:50:16 -0400

    rerenders were because of unnecessary subscription in ChessBoard

--- src/components/ChessBoard/ChessBoard.tsx
+++ src/components/ChessBoard/ChessBoard.tsx
@@ -8,12 +8,9 @@ import { DraggedPiece } from '../DraggedPiece';
 type Props = {};
 
 export function ChessBoard({}: Props) {
-	const piece1 = useAppSelector(state => state.draggedPiece.name);
 	const board = useAppSelector(state => state.game.board);
 
 	const [boardState, setBoardState] = useState<string[][]>(board);
-
-	console.log({ piece1, rook: boardState[7] && boardState[7][7] });
 
 	return (
 		<ChessBoardGrid id="ChessBoard">
